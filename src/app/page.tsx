5fd0049fed6c45042c13cce202566c4a4eda7d58
= <harifin857@outlook.com>
2025-07-29 23:20:35 +0700

    05: ai-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -17,13 +20,14 @@ const Page = () => {
   );
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         disabled={invoke.isPending}
         className={cn("w-full", invoke.isPending && "loading")}
         variant="outline"
         onClick={() =>
           invoke.mutate({
-            text: "Yogi",
+            value,
           })
         }
       >
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,21 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    // Imagine this is a download step
-    await step.sleep("wait-a-moment", "100s");
-    // Imagine this is a download step
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js.  You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+    return { output };
   }
 );
+// 1:58:03
